3127cdb67022c3bf8786b786a44d20265cbb78f8
hanjiarong-tech <2018213326@bupt.edu.cn>
2024-02-29 18:43:24 +0800

    上传视频

--- js/gallery.js
+++ js/gallery.js
@@ -2,11 +2,41 @@ const allPhotos = [
     {
         name: "白马塔顶",
         date: "2023.07.26",
-        description: "",
+        description: "敦煌",
     },
     {
         name: "无界",
-        date: "2023.07.26",
+        date: "2023.07.27",
         description: "戈壁建筑",
+    },
+    {
+        name: "大地之子",
+        date: "2023.07.27",
+        description: "戈壁建筑",
+    },
+    {
+        name: "神殿2",
+        date: "2023.07.27",
+        description: "戈壁建筑",
+    },
+    {
+        name: "阳关1",
+        date: "2023.07.27",
+        description: "阳关",
+    },
+    {
+        name: "阳关小推车",
+        date: "2023.07.27",
+        description: "阳关",
+    },
+    {
+        name: "小黄车",
+        date: "2023.07.27",
+        description: "雅丹",
+    },
+    {
+        name: "雅丹日落",
+        date: "2023.07.27",
+        description: "雅丹",
     },
 ]
